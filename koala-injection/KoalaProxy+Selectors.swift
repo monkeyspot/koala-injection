fca0d033ef40615bcc8bb53a17823196db435a13
Oliver Letterer <oliver.letterer@gmail.com>
2019-03-05 19:19:51 +0100

    Refining selector support.

--- koala-injection/KoalaProxy+Selectors.swift
+++ koala-injection/KoalaProxy+Selectors.swift
@@ -8,34 +8,40 @@
 
 import Foundation
 
+private extension Selector {
+    var koala: String {
+        return self.description.replacingOccurrences(of: ":", with: "")
+    }
+}
+
 public extension KoalaProxy where Location: Before {
     public subscript(selector: Selector) -> (@escaping () -> Void) -> Void {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
     
     public subscript(selector: Selector) -> (@escaping (Type) -> Void) -> Void {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
     
     public subscript(selector: Selector) -> (@escaping (Type, Invocation<Location>) -> Void) -> Void {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
 }
 
 public extension KoalaProxy where Location: After {
     public subscript<T>(selector: Selector) -> (@escaping () -> T) -> Void where T: Any {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
 
     public subscript<T>(selector: Selector) -> (@escaping (Type) -> T) -> Void where T: Any {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
 
     public subscript<T>(selector: Selector) -> (@escaping (Type, Invocation<Location>) -> T) -> Void where T: Any {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
 
     public subscript<T>(selector: Selector) -> (@escaping (Type, Invocation<Location>, T) -> T) -> Void where T: Any {
-        return self[dynamicMember: selector.description.replacingOccurrences(of: ":", with: "")]
+        return self[dynamicMember: selector.koala]
     }
 }
